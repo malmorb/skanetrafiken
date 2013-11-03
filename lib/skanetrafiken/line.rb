5c09018000d8c4a3d0f0a313fd4ce0a09cb03a30
Lars Krantz <lars.krantz@alaz.se>
2013-11-03 22:13:36 +0100

    Refactor: DTOs Line and Point as structs

--- lib/skanetrafiken/line.rb
+++ lib/skanetrafiken/line.rb
@@ -1,29 +1,10 @@
 # encoding: utf-8
 module Skanetrafiken
-  class Line
-    attr_reader :name,:no,:journey_date_time,:is_timing_point,\
+  class Line < Struct.new(:name,:no,:journey_date_time,:is_timing_point,\
       :stop_point,:line_type_id,:line_type_id,:line_type_name,\
-      :towards
+      :towards)
 
-    def initialize dic
-      self.class.properties.each do |key|
-        val = dic[key]
-        instance_variable_set("@#{key.to_s}", val)
-      end
-    end
-
-    def self.properties
-      [:name,:no,:journey_date_time,:is_timing_point,\
-      :stop_point,:line_type_id,:line_type_id,:line_type_name,\
-      :towards]
-    end
-
-    def ==(another)
-      self.class.properties.map do |key|
-        k = "@#{key.to_s}"
-        instance_variable_get(k) == another.instance_variable_get(k)
-      end.inject(true) { |result, element| result and element }
-    end
+    include StructInitializer
 
   end
 end
--- lib/skanetrafiken/point.rb
+++ lib/skanetrafiken/point.rb
@@ -1,32 +1,12 @@
 # encoding: utf-8
 module Skanetrafiken
-
-  $point_indexes = [:stop, :address, :poi, :unknown]
-
-  class Point
-    attr_reader :name, :id, :type
-
-    def initialize dic
-      self.class.properties.each{ |key|
-         val = dic[key]
-         instance_variable_set("@#{key.to_s}", val)
-      }
-    end
-
-    def self.properties
-      [:name, :id, :type]
-    end
-
-    def ==(another_point)
-      Line.properties.map{ |key|
-        k = "@#{key.to_s}"
-        instance_variable_get(k) == another_point.instance_variable_get(k)
-      }.inject(true){ |result, element| result and element }
-    end
-
+  class Point < Struct.new(:name, :id, :type)
+    POINT_INDEXES = [:stop, :address, :poi, :unknown]
+    include StructInitializer
+    
     def render()
-      idx = $point_indexes.index(@type)
-      "#{@name}|#{@id}|#{idx}"
+      idx = POINT_INDEXES.index(type)
+      "#{name}|#{id}|#{idx}"
     end
   end
 
