9a1ad98d8c1550086d898dd48f91a7bda417b39f
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-02-29 12:03:39 +0100

    Update to work on supported rubies

--- lib/skanetrafiken/line.rb
+++ lib/skanetrafiken/line.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 module Skanetrafiken
   class Line < Struct.new(:name,:no,:journey_date_time,:is_timing_point,\
-      :stop_point,:line_type_id,:line_type_id,:line_type_name,\
+      :stop_point,:line_type_id,:line_type_name,\
       :towards)
 
     include StructInitializer
